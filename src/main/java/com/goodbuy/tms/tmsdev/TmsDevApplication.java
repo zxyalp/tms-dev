dfc89b02305086063f8ebb75b80f0617c3853f4c
zxy <orcexzy@163.com>
2019-03-29 18:04:59 +0800

    first commit

---
+++ src/main/java/com/goodbuy/tms/tmsdev/TmsDevApplication.java
@@ -0,0 +1,13 @@
+package com.goodbuy.tms.tmsdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmsDevApplication.class, args);
+    }
+
+}
